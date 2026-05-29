7ec3ef09d579d33ac04ef457e82d7bf553d6db2a
amemajt1 <ameliamajtczak@gmail.com>
2026-05-30 01:47:37 +0200

    worked on main checking function an created overall plan for the project

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #Password Strength Checker
+#Amelia Majtczak
+#This program checks the strength of a user's password and gives feedback on how strong it is and how to
+# improve it if the password is weak.
+
+def check_password_strength(password):
+    strength = 0 
+
+    #Upper letter check
+
+    #Lower letter check
+
+    #Number check
+
+    #Special character check 
+
+    #Lenght check
+
+    # if statements to decide either password is weak, medium, or strong 
+    
+
+def main():
+    password = input("Enter your password: ")
+    strength = check_password_strength(password)
+
+    #If statements to print out if the password is weak, medium, or strong 
+    #and give feedback on what is missing and how to improve if it is medium or weak.
+
+main()
